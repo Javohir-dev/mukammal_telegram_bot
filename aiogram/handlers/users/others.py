c5c77df47007ac99163bdeef1f1d769c05949532
Javohir-dev <javohirblogger7@gmail.com>
2024-04-04 09:09:27 +0500

    remove

--- aiogram/handlers/users/others.py
+++
@@ -1,9 +0,0 @@
-from aiogram import types
-from aiogram.dispatcher import filters
-
-from loader import dp, bot
-
-
-@dp.message_handler(is_reply=True, commands="user_id")
-async def hendle_reply(message: types.Message):
-    await message.answer(message.reply_to_message.from_user.id)
