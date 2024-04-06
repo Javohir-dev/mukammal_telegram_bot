fc9d7352ae1bf80778a0611e311410b6560167f3
Javohir-dev <javohirblogger7@gmail.com>
2024-04-07 00:29:25 +0500

    Lesson: Inline Keyboard Button && Callback Data

--- inline_keyboards/handlers/users/start.py
+++ inline_keyboards/handlers/users/start.py
@@ -1,7 +1,7 @@
 import logging
 
 from aiogram import types
-from aiogram.dispatcher.filters.builtin import CommandStart
+from aiogram.dispatcher.filters.builtin import CommandStart, Text
 
 from keyboards.default.startMenuKeyboard import menuStart
 from loader import dp
@@ -14,3 +14,12 @@ async def bot_start(message: types.Message):
         f"Assalomu alaykum, {message.from_user.full_name}!", reply_markup=menuStart
     )
     await message.delete()
+
+
+@dp.callback_query_handler(Text(equals="main"))
+async def call_bot_start(call: types.CallbackQuery):
+    # logging.info(call)
+    await call.message.answer(
+        f"Assalomu alaykum, {call.from_user.full_name}!", reply_markup=menuStart
+    )
+    await call.message.delete()
